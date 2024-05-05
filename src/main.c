08dc1654139307c78e0b35c14fa579e01dcef87b
Felipe Matias <74963264+Zibec@users.noreply.github.com>
2024-05-05 20:11:53 +0000

    Bala

--- src/main.c
+++ src/main.c
@@ -14,42 +14,39 @@
 int x = 34, y = 22;
 int incX = 1, incY = 1;
 int bulletX = -1, bulletY = -1;
+int bulletSpeed = 100000;
 
 void printHello(int nextX, int nextY) {
   screenSetColor(CYAN, DARKGRAY);
   screenGotoxy(x, y);
-  printf("           ");
+  printf("            ");
 
   screenGotoxy(x, y);
   printf("           ");
 
-  printf("Nave");
+  printf(" △ ");
+
   printf("           ");
 }
 
-void printKey(int ch) {
-  screenSetColor(YELLOW, DARKGRAY);
-  screenGotoxy(35, 22);
-
-  screenGotoxy(34, 23);
-  printf("            ");
-
-  if (ch == 27)
-    screenGotoxy(36, 23);
-  else
-    screenGotoxy(39, 23);
-
-  printf("%d ", ch);
-  while (keyhit()) {
-    printf("%d ", readch());
-  }
-}
 
 void printBullet() {
   if (bulletX != -1 && bulletY != -1) { // Se houver uma bala
     screenSetColor(YELLOW, DARKGRAY);
     screenGotoxy(bulletX, bulletY);
-    printf("^");
+
+    for (int i = 0; i < 10; i++) {
+      screenGotoxy(bulletX, bulletY - i);
+      printf("^");
+      usleep(bulletSpeed);
+      screenUpdate();
+      screenGotoxy(bulletX, bulletY - i);
+      printf("  ");
+
+     
+    }
+    bulletX = -1;
+    bulletY = -1;
   }
 }
 
@@ -70,12 +67,9 @@ int main() {
       ch = readch();
 
       if (ch == ' ') {
-        for (int i = 0; i < 10; i++) {
-
-          bulletX = x + 13;
-          bulletY = y - i;
-
-        }
+        bulletX = x + 13;
+        bulletY = y - 1;
+        printBullet();
       }
       if (ch == 'a' || ch == 'A') {
         x -= 1; // Move a nave para a esquerda
@@ -83,8 +77,6 @@ int main() {
         x += 1; // Move a nave para a direita
       }
 
-      printKey(ch);
-      printBullet();
       screenUpdate();
     }
 
@@ -97,7 +89,6 @@ int main() {
       if (newY >= MAXY - 1 || newY <= MINY + 1)
         incY = -incY;
 
-      printKey(ch);
       printHello(newX, newY);
 
       screenUpdate();
